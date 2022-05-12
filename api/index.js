57f4781cf0bc686e3af577f1ddc66179bacfbff4
luiz Boldrini <luizboldriniej@gmail.com>
2022-05-12 17:04:02 -0300

    Init API

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 3000
+
+app.get('/teste', (req, res) => res.status(200).send({ mensagem: 'Api funcionando!' }))
+
+app.listen(port, () => console.log(`Api rodando na porta ${port}`))
+
+module.exports = app
